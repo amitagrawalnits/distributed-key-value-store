da3b8881c5a8b4f6baeeeabfd71ca56726688113
Amit <amit.agrawal@314ecorp.com>
2025-11-30 16:47:37 +0530

    Initial Commit

---
+++ distributed-key-value-store/src/main/java/com/amit/cs/distributedkeyvaluestore/DistributedKeyValueStoreApplication.java
@@ -0,0 +1,13 @@
+package com.amit.cs.distributedkeyvaluestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedKeyValueStoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DistributedKeyValueStoreApplication.class, args);
+  }
+
+}
---
+++ distributed-key-value-store/src/test/java/com/amit/cs/distributedkeyvaluestore/DistributedKeyValueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.cs.distributedkeyvaluestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedKeyValueStoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
